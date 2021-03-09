aa560aa8e1b0c09e33e636d76d51baa913f0c143
Edvards.T.M <BasistaPlay@gmail.com>
2021-03-09 14:35:09 +0200

    save_5

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+const books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-console.log(book);
+book.textContent += ' (Book title)';
 });
+
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Grāmatas un citas grāmatas...</h2>';
+bookList.innerHTML += '<p>Šādi jūs pievienojat HTML saturu</p>';
