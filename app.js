3bc98140f9935a2a597e04ba397f593da763ed7c
Edvards.T.M <BasistaPlay@gmail.com>
2021-03-09 14:42:00 +0200

    save_9

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const bookList = document.querySelector('#book-list');
 
-console.log('#book-list next sibling:', bookList.nextSibling);
-console.log('#book-list next element sibling:', bookList.nextElementSibling);
-console.log('#book-list previous sibling:', bookList.previousSibling);
-console.log('#book-list previous element sibling:', bookList.previousElementSibling);
+const listItems = document.querySelectorAll('#book-list ul li');
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Pārāk forši visiem pārējiem!';
+Array.from(listItems).forEach(function(item){
+item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+});
+});
+
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+e.preventDefault();
+console.log('Navigation to', e.target.textContent, 'was prevented');
+});
