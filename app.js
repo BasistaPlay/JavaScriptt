7518a25aa36a7fc158d9d14378721f8c524f571f
Edvards.T.M <BasistaPlay@gmail.com>
2021-03-09 14:43:51 +0200

    save_10

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
+const list = document.querySelector('#book-list ul');
 
-const listItems = document.querySelectorAll('#book-list ul li');
-
-Array.from(listItems).forEach(function(item){
-item.addEventListener('click', (e) => {
-
+// delete books
+list.addEventListener('click', (e) => {
+if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
     li.parentNode.removeChild(li);
-
-});
-});
-
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-e.preventDefault();
-console.log('Navigation to', e.target.textContent, 'was prevented');
+}
 });
